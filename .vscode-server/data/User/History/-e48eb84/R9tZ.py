7cb0eadac118aebd4fc3dfe2c2f43945e0bbbf1d
lrb96 <alrojasb96@hotmail.com>
2022-04-21 20:35:41 -0500

    filter,map,reduce

--- .vscode-server/data/User/History/-e48eb84/R9tZ.py
+++ .vscode-server/data/User/History/-e48eb84/R9tZ.py
@@ -4,7 +4,7 @@ def run():
     # my_dict = {i: i**3  for i in range(1,101) if i%3 != 0}
     # print(my_dict)
 
-    my_dict2 = {i: round(math.sqrt(i), 4) for i in range(0,1001)}
+    my_dict2 = {math.sqrt(i) for i in range(0,1001)}
     print(my_dict2)
 
 if __name__ == "__main__":
